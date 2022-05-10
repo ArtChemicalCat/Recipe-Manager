b498f95c69f205b6650fab59886c71232dd4e99a
ArtChemicalCat <93403722+ArtChemicalCat@users.noreply.github.com>
2022-05-10 16:26:57 +0300

    Initial Commit

---
+++ Recipe Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recipe Manager
+//
+//  Created by Николай Казанин on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
